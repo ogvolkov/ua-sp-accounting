97959301fa27b5222ab0cff3680ad56f896d764e
Oleg Volkov <OGVolkov@gmail.com>
2016-10-14 18:42:25 +0300

    Account for quirky NBU behavior between 04.04.2014 and 31.03.2015

--- src/ExchangeRate.fs
+++ src/ExchangeRate.fs
@@ -6,7 +6,13 @@ open System.Xml.Linq
 
 let getNBUrate (date: DateTime) currencyCode =
     let httpClient = new WebClient()
-    let url = String.Format("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={1}&date={0:yyyyMMdd}", date, currencyCode)
+    // between 04.04.204 and 31.03.2015, NBU announced exchange rate in the middle of the day
+    // hence take the previous day's value because currency operations are done before noon    
+    // http://www.bank.gov.ua/control/uk/publish/article?art_id=6629632&cat_id=55838
+    // https://bank.gov.ua/control/uk/publish/article?art_id=15821785&cat_id=55838
+    let exchangeRateDate = if (date >= new DateTime(2014, 4, 4) && date <= new DateTime(2015, 03, 31)) then date.AddDays(-1.0) else date
+
+    let url = String.Format("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={1}&date={0:yyyyMMdd}", exchangeRateDate, currencyCode)
     
     let response = httpClient.DownloadString url
 
